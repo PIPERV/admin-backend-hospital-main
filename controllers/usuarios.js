5833806bcc8b4c4c5f84cc7a11ae49815d3de636
Andres Felipe <arodriguezv1@yahoo.com>
2022-07-19 10:47:11 -0500

    Final sección 11

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -7,11 +7,29 @@ const {generarJWT} = require('../helpers/jwt');
 
 const getUsuarios = async(req, res)=>{
 
-    const usuario = await Usuario.find({}, 'nombre email role google');
+    const desde = Number(req.query.desde) || 0;
+    
+    //  console.log(desde);
+    //  const usuarios = await Usuario
+    //                          .find({}, 'nombre email role google')
+    //                          .skip( desde )
+    //                          .limit( 5 );
+    
+    // const total = await Usuario.count();
+
+    const [ usuarios, total ] = await Promise.all([
+        Usuario
+          .find({}, 'nombre email role google img')
+          .skip( desde )
+          .limit( 5 ),
+
+        Usuario.countDocuments(),
+    ]);
 
     res.json({
         ok:true,
-        usuario
+        usuarios,
+        total
     });
 }
 
